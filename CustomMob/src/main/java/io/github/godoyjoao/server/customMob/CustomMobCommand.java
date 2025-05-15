89438b59cf498797032db6de45f1b587c8bf2a8a
João Godoy <joaoguilherme-est@hotmail.com>
2025-05-14 21:19:42 -0300

    plugin update

--- CustomMob/src/main/java/io/github/godoyjoao/server/customMob/CustomMobCommand.java
+++ CustomMob/src/main/java/io/github/godoyjoao/server/customMob/CustomMobCommand.java
@@ -1,5 +1,7 @@
 package io.github.godoyjoao.server.customMob;
 
+import com.destroystokyo.paper.Namespaced;
+import com.google.common.collect.Multimap;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.arguments.FloatArgumentType;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
@@ -9,13 +11,28 @@ import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.Commands;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.TextColor;
-import org.bukkit.Location;
-import org.bukkit.World;
+import net.md_5.bungee.api.chat.BaseComponent;
+import org.bukkit.*;
 import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.command.CommandSender;
+import org.bukkit.damage.DamageType;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Zombie;
+import org.bukkit.inventory.*;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.components.*;
+import org.bukkit.inventory.meta.tags.CustomItemTagContainer;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class CustomMobCommand {
 
@@ -42,6 +59,17 @@ public class CustomMobCommand {
         zombie.setCustomNameVisible(true);
         zombie.getAttribute(Attribute.MAX_HEALTH).setBaseValue(50.0);
         zombie.setHealth(50.0);
+        EntityEquipment equipment = zombie.getEquipment();
+        ItemStack diamondChestplate = new ItemStack(Material.DIAMOND_CHESTPLATE);
+        ItemMeta meta = diamondChestplate.getItemMeta();
+        meta.addEnchant(Enchantment.UNBREAKING, 100, true);
+        meta.addEnchant(Enchantment.PROTECTION, 10, true);
+        meta.addEnchant(Enchantment.THORNS, 15, true);
+        diamondChestplate.setItemMeta(meta);
+        equipment.setChestplate(diamondChestplate);
+        for (ItemStack item : equipment.getArmorContents()) {
+            ctx.getSource().getSender().sendMessage(item.displayName());
+        }
 
         return Command.SINGLE_SUCCESS;
     }
